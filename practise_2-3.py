8be168443bcb9707ff07a66b3db0ed31114a55f1
Wansen Song <2812483751@qq.com>
2023-09-27 22:38:32 +0800

    进度更新

--- practise_2-3.py
+++ practise_2-3.py
@@ -4,3 +4,5 @@ print(f"大写{name.upper()} 小写{name.lower()} 首字母大写{name.title()}"
 star = "Albert Einstein"
 word = 'A person who never made a mistake never tried anything new.'
 print(f'{star} onec said, "{word}"')
+a_man = " henry morgan "
+print(f"{a_man}\t{a_man.lstrip()}\n{a_man.rstrip()}\t{a_man.strip()}")
---
+++ practise_2-8.py
@@ -0,0 +1,4 @@
+print(2 ** 3)
+print(9 - 1)
+print(1 * 8)
+print((int)(64 / 8))
