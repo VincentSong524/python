bf90fa9346ad77534c021ca5902541513d190d57
Wansen Song <2812483751@qq.com>
2023-09-27 10:03:54 +0800

    nothing

---
+++ apostrophe.py
@@ -0,0 +1,2 @@
+message = 'One of python's strengths is its diverse communit.
+print(message) # 单引号的字符串继续使用单引号作为内容会出错
---
+++ name.py
@@ -0,0 +1,9 @@
+name = "ada lobelace"
+print(name)
+print(name.title())
+print("Python")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+favorite_language = "python "
+print(f"{favorite_language}.")
+favorite_language.rstrip()
+print(f"{favorite_language.rstrip()}.")
