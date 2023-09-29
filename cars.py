7a63a5e59224c34d5e3a40a8dd6112825192a2c5
Wansen Song <2812483751@qq.com>
2023-09-30 00:40:08 +0800

    依旧列表操作

--- cars.py
+++ cars.py
@@ -4,3 +4,19 @@ print(cars)
 
 cars.sort(reverse=True)#反向排序
 print(cars)
+
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list:")
+print(cars)
+
+print('\nHere is the sorted list:')
+print(sorted(cars))#临时排序
+
+print('\nHere is the original list again:')
+print(cars)
+
+cars.reverse()
+print(cars)
+cars.reverse()
+print(cars)
---
+++ practice3-8.py
@@ -0,0 +1,21 @@
+vacationPlace = ['Qingdao', 'Jinan', 'Wuhan', 'Hongkong', 'Hangzhou']
+
+print(vacationPlace)
+
+print(sorted(vacationPlace))
+print(vacationPlace)
+
+print(sorted(vacationPlace, reverse=True))
+
+vacationPlace.reverse()
+print(vacationPlace)
+
+vacationPlace.reverse()
+print(vacationPlace)
+
+vacationPlace.sort()
+print(vacationPlace)
+
+vacationPlace.sort(reverse=True)
+print(vacationPlace)
+
