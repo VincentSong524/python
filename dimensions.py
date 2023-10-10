b9a36176c53d747792f0202064ad067b3dc69f40
Wansen Song <2812483751@qq.com>
2023-10-10 15:59:18 +0800

    数组

---
+++ dimensions.py
@@ -0,0 +1,15 @@
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+#try change some element
+#dimensions[0] = 250
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
---
+++ practice4-13.py
@@ -0,0 +1,11 @@
+foods = ("水饺", "面条", "鸡公煲", "麻辣烫", "豆腐脑")
+for food in foods:
+    print(food)
+
+# foods[1] = "小酥肉"
+
+print("\nNew menu.\n")
+
+foods = ("小笼包", "黄焖鸡", "鸡公煲", "麻辣烫", "豆腐脑")
+for food in foods:
+    print(food)
