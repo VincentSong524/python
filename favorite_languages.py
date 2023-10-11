9e21d628b929ea0e9f780f63cefd3035c7ee9e75
Wansen Song <2812483751@qq.com>
2023-10-11 18:43:44 +0800

    字典嵌套列表

--- favorite_languages.py
+++ favorite_languages.py
@@ -1,15 +1,15 @@
 import my_tools
 
 favorite_languages = {
-    'jen': 'python',
-    'sarah': 'c',
-    'edward': 'ruby',
-    'phil': 'python',
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
 }
-
-language = favorite_languages['sarah'].title()
+"""
+language = favorite_languages['sarah'][0].title()
 print(f"Sarah's favotite language is {language}.")
-
+"""
 my_tools.line()
 
 for name, language in favorite_languages.items():
@@ -26,7 +26,7 @@ for name in favorite_languages:
     print(name.title())
 
 my_tools.line()
-
+"""
 friends = ['phil', 'sarah']
 for name in favorite_languages.keys():
     print(f"Hi {name.title()}")
@@ -37,11 +37,12 @@ for name in favorite_languages.keys():
 
 if 'erin' not in favorite_languages.keys():
     print("Erin, please take our poll!")
-
+"""
 my_tools.line()
-
+"""
 for language in set(favorite_languages.values()):
     print(language)
+"""
 
 my_tools.line()
 
@@ -52,3 +53,13 @@ for name in need_poll:
         print(f"{name.title()} thank you for participating in the survey.")
     else:
         print(f"{name.title()}, please take our poll!")
+
+my_tools.line()
+
+for name, languages in favorite_languages.items():
+    if len(languages) == 1:
+        print(f"\n{name.title()}'s favorite language is {languages[0]}.")
+    else:
+        print(f"\n{name.title()}'s favorite languages are:")
+        for language in languages:
+            print(f"\t{language.title()}")
