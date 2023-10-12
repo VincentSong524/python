12052ffe48528e25e271e5802790fe4616f61c34
Wansen Song <2812483751@qq.com>
2023-10-12 10:27:57 +0800

    列表内嵌套字典并遍历输出

--- practice6-1.py
+++ practice6-1.py
@@ -1,6 +1,31 @@
-friend = {
+friend_0 = {
     'first_name': 'henry',
     'last_name': 'morgan',
     'age': '23',
     'place_city': 'New York',
 }
+
+friend_1 = {
+    'first_name': 'henry',
+    'last_name': 'morgan',
+    'age': '23',
+    'place_city': 'New York',
+}
+
+friend_2 = {
+    'first_name': 'henry',
+    'last_name': 'morgan',
+    'age': '23',
+    'place_city': 'New York',
+}
+
+people = [
+    friend_0,
+    friend_1,
+    friend_2
+]
+
+for info in people:
+    for key, value in info.items():
+        print(f"{key}: {value}")
+    print()
