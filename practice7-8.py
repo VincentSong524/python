9dd712fb8990d83a9b7607870d5b7c2c3eaccacc
Wansen <2812483751@qq.com>
2023-10-13 10:21:07 +0800

    使用循环多次删除列表内的特定元素

--- practice7-8.py
+++ practice7-8.py
@@ -1,4 +1,4 @@
-sandwich_orders = ['sandwich_0', 'sandwich_1', 'sandwich_2']
+sandwich_orders = ['sandwich_0', 'pastrami', 'sandwich_1', 'pastrami', 'sandwich_2', 'pastrami']
 finished_sandwiches = []
 
 for sandwich in sandwich_orders:
@@ -7,3 +7,7 @@ for sandwich in sandwich_orders:
     #sandwich_orders.remove(sandwich)#我认为这里的删除导致了的原列表顺序的改变，导致了程序的错误
 
 print(finished_sandwiches)
+
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+print(sandwich_orders)
