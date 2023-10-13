db6ca5d1d0174cc8238bb3ac7b1f18940c57c8bd
Wansen <2812483751@qq.com>
2023-10-13 18:27:05 +0800

    函数与参数

---
+++ practice8-3.py
@@ -0,0 +1,8 @@
+def make_shirt(size, word='I love Python'):
+    print(f"The T-shirt size is {size} and word is {word}.")
+
+if __name__ == "__main__":
+    make_shirt(18, 'green_tree')
+    make_shirt(size='18', word='red_blod')
+    make_shirt(size='big')
+    make_shirt(size='medium')
