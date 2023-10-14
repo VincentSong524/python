b2c6893674f203785a7c4672946646ccb5be0ebe
Wansen Song <2812483751@qq.com>
2023-10-14 23:38:16 +0800

    使用 *型参名 传递任意数量的实参

--- pizza.py
+++ pizza.py
@@ -10,3 +10,10 @@ print(f"You ordered a {pizza['crust']}-crust pizza "
 
 for topping in pizza['toppings']:
     print("\t" + topping)
+
+def make_pizza(*toppings):
+    """打印顾客点的所有配料"""
+    print(toppings)
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
