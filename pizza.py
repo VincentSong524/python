33722268ed17e2b5f67ed71797ff4d13cc5e21dc
Wansen Song <2812483751@qq.com>
2023-10-14 23:39:40 +0800

    使用循环来遍历元组

--- pizza.py
+++ pizza.py
@@ -13,7 +13,8 @@ for topping in pizza['toppings']:
 
 def make_pizza(*toppings):
     """打印顾客点的所有配料"""
-    print(toppings)
+    for topping in toppings:
+        print(topping)
 
 make_pizza('pepperoni')
 make_pizza('mushrooms', 'green peppers', 'extra cheese')
