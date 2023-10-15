28eaa7d7e10cac55cfb7d3dbc9d84efdf36aa3d1
Wansen Song <2812483751@qq.com>
2023-10-15 09:39:51 +0800

    三种导入模块的方式

--- 8-6-1/makeing_pizzas.py
+++ 8-6-1/makeing_pizzas.py
@@ -1,4 +1,6 @@
-from pizza import make_pizza
+#import pizza    #导入pizza
+from pizza import make_pizza    #从pizza导入make_pizza
+#from pizza import make_pizza as mp  #从pizza导入make_pizza并重命名为mp
 
 make_pizza(16, 'peeperoni')
 make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
