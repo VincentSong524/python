c3c0b59ab047f9a1c9044b15953f31e9375a9e0d
Wansen <2812483751@qq.com>
2023-10-16 17:23:36 +0800

    class

---
+++ chapter_9/car.py
@@ -0,0 +1,51 @@
+class Car:
+    """一次模拟汽车的简单尝试"""
+    
+    def __init__(self, make, model, year) -> None:
+        """"初始化描述汽车的属性。"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    
+    def get_descriptive(self):
+        """返回整洁的描述性信息"""
+
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name
+    
+
+    def read_odometer(self):
+        """打印一条指出汽车里程的消息。"""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    
+    def update_odometer(self, mileage):
+        """
+        将里程表读数设置为指定的值。
+        禁止将里程表读数往回调。
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+
+    def increment_ofometer(self, miles):
+        """将里程表读数增加指定的量"""
+        if miles >= 0:
+            self.odometer_reading += miles
+        else:
+            print("You can't roll back an odometer!")
+            
+
+my_new_car = Car('audi', 'a4', 2019)
+print(my_new_car.get_descriptive())
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+my_new_car.increment_ofometer(23_500)
+my_new_car.read_odometer()
--- chapter_9/practice9-1.py
+++ chapter_9/practice9-1.py
@@ -1,11 +1,12 @@
 class Restaurant:
     """餐馆类"""
 
-    def __init__(self, restaurant_name, cuisine_type) -> None:
+    def __init__(self, restaurant_name, cuisine_type, number_serverd=0) -> None:
         """初始化餐馆名称与烹饪类型"""
 
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_serverd = number_serverd
     
 
     def describe_restaurant(self):
@@ -19,9 +20,18 @@ class Restaurant:
         
         print("restaurant is open!")
 
+    
+    def set_number_serverd(self, number_serverd):
+        """修改就餐人数并打印"""
+        self.number_serverd = number_serverd
+        print(f"{self.number_serverd} people came here for a meal.")
+
 
 kfc = Restaurant("KFC", 'Fried chicken')
 kfc.describe_restaurant()
 
 dicos = Restaurant("Dicos", "Fried chicken")
 dicos.describe_restaurant()
+
+how_many_get = Restaurant('KFC', 'Fried chicken', 100)
+print(how_many_get.number_serverd)
