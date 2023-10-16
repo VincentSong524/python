ad36fe19fc708f0d1cc7fcc5badb9f5dec58860a
Wansen <2812483751@qq.com>
2023-10-16 18:42:17 +0800

    增加功能

--- chapter_9/practice9-1.py
+++ chapter_9/practice9-1.py
@@ -11,13 +11,11 @@ class Restaurant:
 
     def describe_restaurant(self):
         """打印两项属性信息"""
-
         print(f"{self.restaurant_name} {self.cuisine_type}.")
 
     
     def open_restaurant(self):
-        """打印餐馆正在营业"""
-        
+        """打印餐馆正在营业""" 
         print("restaurant is open!")
 
     
@@ -25,6 +23,12 @@ class Restaurant:
         """修改就餐人数并打印"""
         self.number_serverd = number_serverd
         print(f"{self.number_serverd} people came here for a meal.")
+
+
+    def increment_number_servverd(self, number_serverd):
+        """递增就餐人数"""
+        if number_serverd >= 0:
+            self.number_serverd += number_serverd
 
 
 kfc = Restaurant("KFC", 'Fried chicken')
--- chapter_9/practice9-3.py
+++ chapter_9/practice9-3.py
@@ -3,21 +3,30 @@ class User:
 
     def __init__(self, first_name, last_name) -> None:
         """初始化名字"""
-
         self.first_name = first_name
         self.last_name = last_name
+        self.login_attempts = 0
 
     
     def describe_user(self):
         """打印用户信心摘要"""
-
         print(f"First name:{self.first_name}\tLast name:{self.last_name}")
     
 
     def greet_user(self):
         """个性化问候"""
-
         print(f"Hello, {self.first_name}.")
+
+
+    def increment_login_attempts(self):
+        """记录登录次数"""
+        self.login_attempts += 1
+
+
+    def reset_login_attempts(self):
+        """重置 login_attempts 的值为0。"""
+        self.login_attempts = 0
+
 
 #henry
 henry = User('Henry', 'Morgan')
@@ -28,3 +37,12 @@ henry.greet_user()
 vincent = User('Vincent', "Song")
 vincent.describe_user()
 vincent.greet_user()
+
+#practice9-5
+song = User('wansen', 'song')
+song.increment_login_attempts()
+song.increment_login_attempts()
+song.increment_login_attempts()
+print(f"{song.login_attempts}")
+song.reset_login_attempts()
+print(f"{song.login_attempts}")
