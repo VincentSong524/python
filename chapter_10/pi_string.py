43883bda9674ae2d29d506b7a5eadb23d093a02f
Wansen <2812483751@qq.com>
2023-10-18 16:05:52 +0800

    文件处理

--- chapter_10/pi_string.py
+++ chapter_10/pi_string.py
@@ -1,16 +1,11 @@
-file_name = '/home/ubuntu/python/chapter_10/pi_digits.txt'
-name = 'pi_digits.txt'
+file_name = 'pi_million_digits.txt'
 
 with open(file_name) as file_object:
     lines = file_object.readlines()
-
-for line in lines:
-    print(line.rstrip())
-
 
 pi_string = ''
 for line in lines:
     pi_string += line.strip()
 
-print(pi_string)
+print(f"{pi_string[:52]}...")
 print(len(pi_string))
