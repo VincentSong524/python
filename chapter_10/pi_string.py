d410a909a30fef09b5ccc5fbc50bb64871c33746
Wansen <2812483751@qq.com>
2023-10-18 16:45:48 +0800

    使用拼接绝对路径方式都可以正常运行 但为什么使用相对路径就会报错呢？

--- chapter_10/pi_string.py
+++ chapter_10/pi_string.py
@@ -1,6 +1,15 @@
+import os
+
+#获取脚本文件的绝对路径
+script_dir = os.path.dirname(__file__)
+
+#要处理的文件名称
 file_name = 'pi_million_digits.txt'
 
-with open(file_name) as file_object:
+# 构建要处理的文件的相对路径
+file_to_process = os.path.join(script_dir, file_name)
+
+with open(file_to_process) as file_object:
     lines = file_object.readlines()
 
 pi_string = ''
