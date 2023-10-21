181a1a70f57899fe12050df32571526a99344efb
Wansen <2812483751@qq.com>
2023-10-21 19:55:22 +0800

    重构

--- chapter_10/remember_me.py
+++ chapter_10/remember_me.py
@@ -1,21 +1,30 @@
 import json
 
 
-def greet_user():
-    """问候用户，并指出其名字。"""
-
+def get_stored_username():
+    """如果存储了用户名，就获取它。"""
     filename = 'username.json'
     try:
-            
         with open(filename) as f:
             username = json.load(f)
     except FileNotFoundError:
-        username = input("What is your name:")
-        with open(filename, 'w') as f:
-            json.dump(username, f)
-            print(f"We'll remember you when you come vack, {username}!")
+        return None
     else:
-        print(f"Welcome back, {username}")
+        return username
+    
+def greet_user():
+        """问候用户，并指出其名字。"""
+        username = get_stored_username()
+        if username:
+            print(f"Welcome back, {username}")
+        else:
+ 
+            username = input("What is your name:")
+            filename = 'username.json'
+            with open(filename, 'w') as f:
+                json.dump(username, f)
+                print(f"We'll remember you when you come vack, {username}!")
+
 
 
 if __name__ == "__main__":
