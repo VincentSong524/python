39aafdf083b19e5bb99a6c65c48ae176067926ed
Wansen <2812483751@qq.com>
2023-10-22 10:56:46 +0800

    I/O完结

--- chapter_10/remember_me.py
+++ chapter_10/remember_me.py
@@ -11,13 +11,26 @@ def get_stored_username():
     else:
         return username
     
+def get_new_username():
+    """"获取新用户名"""
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        username = input("Please enter your name: ")
+        json.dump(username, f)
+    
 def greet_user():
         """问候用户，并指出其名字。"""
         username = get_stored_username()
+        
         if username:
-            print(f"Welcome back, {username}")
+            print(f"Is your name {username}(yes/no)?")
+            judgment = input()
+            if judgment == 'yes':
+                print(f"Welcome back, {username}")
+            else:
+                get_new_username()
         else:
- 
+            #
             username = input("What is your name:")
             filename = 'username.json'
             with open(filename, 'w') as f:
