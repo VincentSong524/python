1a68034a0bc9060a15b676d69e8489af17a809b8
Wansen <2812483751@qq.com>
2023-10-23 12:31:23 +0800

    nothing

--- chapter_11/test.cities.py
+++ chapter_11/test.cities.py
@@ -2,7 +2,7 @@ import unittest
 from city_functions import city_country
 
 class CitiesTestCase(unittest.TestCase):
-    """测试city_functions.py"""
+    """测试 city_functions.py"""
     
     def test_city_country(self):
         """能够正确处理 City, Country这样的格式吗"""
@@ -10,7 +10,7 @@ class CitiesTestCase(unittest.TestCase):
         self.assertEqual(formatted_name, 'City, Country')
 
     def test_city_population(self):
-        """"能够正确处理City, Country - population xxx这样的格式吗"""
+        """"能够正确处理 City, Country - population xxx 这样的格式吗"""
         formatted_name = city_country('city', 'country', '5000000')
         self.assertEqual(formatted_name, 'City, Country - population 5000000')
 
